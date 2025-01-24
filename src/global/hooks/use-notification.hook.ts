faa31979c4232e631f5af4b4a33624155c414b2d
Vikrant Bhurke <bhurkevi@gmail.com>
2025-01-24 16:09:51 +0530

    style notification

--- src/global/hooks/use-notification.hook.ts
+++ src/global/hooks/use-notification.hook.ts
@@ -1,17 +1,25 @@
 import { notifications } from "@mantine/notifications";
 import { NotificationColor } from "../enums/notification-color.enum";
-import { borderLCBorder, oneBg } from "../styles/app.css";
+import { noBorder } from "../styles/app.css";
+import { useMantineColorScheme } from "@mantine/core";
 
 export const useNotification = () => {
+  const theme = useMantineColorScheme();
   const showNotification = (title: string, color: NotificationColor) => {
     notifications.show({
       title,
-      color,
+      withCloseButton: false,
+      color: "white",
       position: "bottom-center",
       message: "",
+      styles: {
+        title: {
+          color: "white",
+        },
+      },
       style: {
-        backgroundColor: oneBg,
-        border: borderLCBorder,
+        backgroundColor: color,
+        border: noBorder,
       },
       autoClose: 5000,
     });
