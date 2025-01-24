cb54c74a1982c44cac58a89729261b4fe8c0db58
Vikrant Bhurke <bhurkevi@gmail.com>
2025-01-24 16:39:30 +0530

    style notification

--- src/global/hooks/use-notification.hook.ts
+++ src/global/hooks/use-notification.hook.ts
@@ -1,10 +1,8 @@
 import { notifications } from "@mantine/notifications";
 import { NotificationColor } from "../enums/notification-color.enum";
 import { noBorder } from "../styles/app.css";
-import { useMantineColorScheme } from "@mantine/core";
 
 export const useNotification = () => {
-  const theme = useMantineColorScheme();
   const showNotification = (title: string, color: NotificationColor) => {
     notifications.show({
       title,
